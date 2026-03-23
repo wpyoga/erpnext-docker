130d4a8681001034d438d1296237b22665edc4eb
William Poetra Yoga <william.poetra@gmail.com>
2026-03-23 14:22:01 +0100

    feat: add volume for archived sites

--- 03-render-compose.sh
+++ 03-render-compose.sh
@@ -10,6 +10,7 @@ docker compose \
   -f ../overrides.project_name.yaml \
   -f ../overrides.network.yaml \
   -f ../overrides.mariadb_version.yaml \
+  -f ../overrides.archived_sites.yaml \
   config \
   -o compose.prebuilt.yaml
 
